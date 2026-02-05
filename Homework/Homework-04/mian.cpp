3e6884bcc3ff942423c8a92f80f73d687a6b4434
Najib Kako <najibkako05@gmail.com>
2026-02-05 14:54:12 -0800

    finished code lol

---
+++ Homework/Homework-04/mian.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+// Were using the iostream and string command (string is so we can have many letters instead of just one char) 
+
+int main() {
+
+    string country; 
+
+    cout << "where do you live?\n";
+    cin >> country;
+
+    cout << "wow! " << country << " is a SHIT place to leave in I am SO SO SO SO sorry for you.\n";
+
+    return 0;
+
+}
+
+// wow im finally understanding without having to write 1000000000 notes for myself
+// ggs im the best coder alive 
+// if youre reading this thats crazy i didnt think you read the notes
+
+
+
+// comment chicken in the response if you actually read this far.. if not i get a extra credit point.
