d3171dc47360e8e51b5f09d6d0e0efaabde712ea
Najib Kako <najibkako05@gmail.com>
2026-02-04 16:05:22 -0800

    Start of homework 3

---
+++ Homework/Homework-03/main.cpp
@@ -0,0 +1,12 @@
+// Hi Professor, you will see a LOT of notes in this code file. This is because I've been struggling to keep up so
+// as im learning how to code im trying to be able to *EXPLAIN* how it works so it can stick in my brain. 
+
+#include <iostream>
+// This allows us to use inputs and outputs (stands for Input Output Stream)
+
+#include <string>
+// This allows us to use the strings (a line of characters)
+
+using namespace std;
+// This allows us to use the standard library without having to type "std::" before every command 
+
