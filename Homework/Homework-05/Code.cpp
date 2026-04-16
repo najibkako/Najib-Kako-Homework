7978076ca721404f9cedc4c435ad10e23aedff70
Najib Kako <najibkako05@gmail.com>
2026-04-15 23:50:59 -0700

    finished code

--- Homework/Homework-05/Code.cpp
+++ Homework/Homework-05/Code.cpp
@@ -1,1 +1,18 @@
-// will do assnment soon..   :) 
+int main() { 
+    char message [100];
+    int vowel_count = 0;
+
+    cout <<"put all characters here with no spaces brotha: ";
+    cin >> message;
+
+    for (int i = 0; message[i] != '\0'; i++) {
+        if (message[i] == 'a' || message[i] == 'e' || message[i] == 'i' || message[i] == 'o' || message[i] == 'u' ||
+            message[i] == 'A' || message[i] == 'E' || message[i] == 'I' || message[i] == 'O' || message[i] == 'U') {
+            vowel_count++;
+        }
+    }
+    
+    cout << "Number of vowels in yo message BOIII: " << vowel_count << endl;
+
+    return 0;
+}
